a9c792949ff7e7aacfff70d6d2ed02a6c7de6b08
marcusbugge <marcus_bugge@hotmail.com>
2022-02-14 19:41:41 +0100

    simple login handling

--- frontend/clientapp/src/components/core/nav/Navbar.js
+++ frontend/clientapp/src/components/core/nav/Navbar.js
@@ -2,24 +2,63 @@ import React from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import Welcome from "../../pages/welcome/Welcome";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  function logout() {
+    localStorage.removeItem("loggedIn");
+    console.log("log out??");
+  }
+
   return (
     <div className="nav">
       <div className="nav-header"></div>
-
-      <Link to="/login">
-        <button className="loginbtn-nav">Login</button>
-      </Link>
+      {localStorage.getItem("loggedIn") ? (
+        ""
+      ) : (
+        <Link to="/login">
+          <button className="loginbtn-nav">Login</button>
+        </Link>
+      )}
 
       <div className="links-cnt">
+        <h1>LEADERBOARD</h1>
         <div className="standings">
-          <Link to="/">Standings</Link>
+          <div className="rank-nav-cnt">
+            <Link to="/">Allsvenskan</Link>
+            <div className="currentrank">#2</div>
+          </div>
+
+          <div className="rank-nav-cnt">
+            <Link to="/">Sweden</Link>
+            <div className="currentrank">#90</div>
+          </div>
+        </div>
+        <h1>COMPETITIONS</h1>
+        <div className="standings">
+          <div className="rank-nav-cnt">
+            <Link to="/">Most popular video</Link>
+          </div>
+
+          <div className="rank-nav-cnt">
+            <Link to="/">Most popular clubsong</Link>
+          </div>
         </div>
         <h1>PROFIL</h1>
         <div className="links">
-          <Link to="/profil">Profil</Link>
-          <Link to="/feed">Feed</Link>
+          <div className="rank-nav-cnt">
+            <Link to="/profil">Profil</Link>
+          </div>
+          <div className="rank-nav-cnt">
+            <Link to="/feed">Feed</Link>
+          </div>
+          {localStorage.getItem("loggedIn") ? (
+            <button onClick={logout}>Log out</button>
+          ) : (
+            ""
+          )}
         </div>
         <div className="a"></div>
       </div>
