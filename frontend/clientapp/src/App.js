971606b79ce196b78abbf5c81f1866c2b56cfa0e
Fredrik Brinch Frydenlund <fredrik.brinch@gmail.com>
2022-02-16 14:10:57 +0100

    Creating the registration for the user page and adding a gui to this page

--- frontend/clientapp/src/App.js
+++ frontend/clientapp/src/App.js
@@ -7,6 +7,7 @@ import Login from "./components/pages/login/Login.js";
 import Profilepage from "./components/pages/profile/Profilepage";
 import FeedPosts from "./components/pages/feed/FeedPosts";
 import MainStandPage from "./components/pages/standings/MainStandPage";
+import CreateUser from "./components/pages/login/CreateUser";
 
 function App() {
   return (
@@ -21,7 +22,9 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/profil" element={<Profilepage />} />
             <Route path="/feed" element={<FeedPosts />} />
+            <Route path="/signup" element={<CreateUser />} />
             <Route path="/" element={<MainStandPage />} />
+
           </Routes>
         </div>
       </div>
--- frontend/clientapp/src/components/pages/login/CreateUser.js
+++ frontend/clientapp/src/components/pages/login/CreateUser.js
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function CreateUser() {
-  return <div></div>;
+  return <div>
+    <section className="signup">
+        <div className="container mt-5">
+          <div className="signup-content">
+            <div className="signup-form">
+              <h2 className="form-title">Sign up</h2>
+              <form className="register-form" id="register-form">
+                <div className="form-group">
+                  <label htmlFor="name">
+                    <i class="xmdi xmdi-account material-icons-name"></i>
+                  </label>
+                  <input type="text" name="name" id="name" autoComplete="off" placeholder="Your name"></input>
+                </div>
+
+              </form>
+            </div>
+          </div>
+        </div>
+
+
+    </section>
+
+  </div>;
 }
