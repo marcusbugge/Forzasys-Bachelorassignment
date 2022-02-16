b8a4964e4287404f98628c3108dd9f10f8c59cd8
marcusbugge <marcus_bugge@hotmail.com>
2022-02-16 14:08:14 +0100

    styling

--- frontend/clientapp/src/components/pages/profile/Profilepage.js
+++ frontend/clientapp/src/components/pages/profile/Profilepage.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./profiledata.css";
 import usericon from "../../../assets/icons/usericon.png";
+import FeedPosts from "../feed/FeedPosts";
 
 export default function Profilepage() {
+  const posts = [
+    {
+      id: 1,
+      url: "www/ksdhfjksdhfkdsk.com",
+      owner: "owner",
+    },
+    {
+      id: 2,
+      url: "www/ksdhfjksdhfkdsk.com",
+      owner: "owner1",
+    },
+    {
+      id: 3,
+      url: "www/ksdhfjksdhfkdsk.com",
+      owner: "owner2",
+    },
+    {
+      id: 4,
+      url: "www/ksdhfjksdhfkdsk.com",
+      owner: "owner3",
+    },
+  ];
+
   return (
-    <div>
+    <div className="profile-cnt">
       <div className="profiledata">
         <div className="picture-icon-cnt">
           <img src={usericon} alt="profilepicture" />
@@ -19,6 +43,24 @@ export default function Profilepage() {
         <div className="badge">4</div>
         <div className="badge">5</div>
         <div className="badge">6</div>
+      </div>
+
+      <div className="most-popularclips-cnt">
+        <div className="stroke-blue"></div>
+        <div className="posts">
+          {posts.map((post, index) => (
+            <div className="post-element" key={index}>
+              <h1>Posted by: {post.owner}</h1>
+              <p>{post.url}</p>
+
+              <div className="buttons">
+                <button>Share</button>
+                <button>Like</button>
+                <button>Play video</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
