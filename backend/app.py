fccc93e2e65aa9673ac4831f2b1c1c5e90a5432a
s345871 <s345871@oslomet.no>
2022-02-21 14:59:52 +0100

    added username and password attribute to User, started with flask_login

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from sqlalchemy import Integer, LargeBinary, String, ForeignKey
 from marshmallow import Schema, fields
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from args import user_put_args, video_put_args, badge_put_args, team_put_args
 
 #https://medium.com/@ns2586/sqlalchemys-relationship-and-lazy-parameter-4a553257d9ef
@@ -14,9 +15,11 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/database'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-
+#-----------------------------------------START OF MODELS ----------------------------------
 earned_badges = db.Table('earned_badges',
     db.Column('user_id', Integer, ForeignKey('user.id')),
     db.Column('badge_id', Integer, ForeignKey('badge.id'))
@@ -30,8 +33,10 @@ followers = db.Table('followers',
 class FollowerSchema(Schema):
     id = fields.Integer()
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(Integer, primary_key=True)
+    username = db.Column(String(30), unique=True, nullable=True)
+    password = db.Column(String(40), nullable=False)
     given_name = db.Column(String(50), nullable=False)
     family_name = db.Column(String(50), nullable=False)
     age = db.Column(Integer, nullable=False)
@@ -81,8 +86,17 @@ class User(db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
+    def is_authenticated(self, username, password):
+        users = User.get_all()
+        for user in users:
+            if username.lower() == user.username.lower() and password == user.password:
+                return True
+        return False
+
 class UserSchema(Schema):
     id = fields.Integer()
+    username = fields.String()
+    password = fields.String()
     given_name = fields.String()
     family_name = fields.String()
     age = fields.Integer()
@@ -96,7 +110,7 @@ class UserSchema(Schema):
 class Team(db.Model):
     id = db.Column(Integer, primary_key=True)
     name = db.Column(String(30), nullable=False)
-    nickname = db.Column(String(5), nullable=False)
+    nickname = db.Column(String(6), nullable=False)
     nationality = db.Column(String(25), nullable=False)
     logo = db.Column(String(250), nullable=False)
     supporters = db.relationship('User', backref=db.backref('database', lazy='joined'), lazy='select')
@@ -207,6 +221,41 @@ class BadgeSchema(Schema):
     category = fields.String()
     points_needed = fields.String()
 
+#--------------------------------------- END OF MODELS ----------------------------------
+
+@login_manager.user_loader
+def load_user(id):
+    return User.get_by_id(id)
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    try:
+        data = request.args
+        user_to_login = User.is_authenticated(data['username'], data['password'])
+        login_user(user_to_login)
+        return jsonify({
+            'message' : 'Logging in...'
+        }), 200
+    except:
+        return jsonify({
+            'error' : 'Wrong username and/ or password'
+        }), 404
+
+@app.route('/api/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({
+        'message' : 'Logging out...'
+    }), 200
+
+@app.route('/api/loggedInUser', methods=['GET'])
+@login_required
+def loggedInUser():
+    serializer = UserSchema()
+    result = serializer.dump(current_user)
+
+    return jsonify(result), 200
 
 @app.route('/api/user', methods=['GET'])
 def get_all_users():
@@ -218,7 +267,10 @@ def get_all_users():
 @app.route('/api/user', methods=['POST'])
 def create_user():
     data = request.args
+    print(data)
     newUser = User(
+        username = data['username'],
+        password = data['password'],
         given_name = data['given_name'],
         family_name = data['family_name'],
         age = data['age'],
@@ -245,6 +297,8 @@ def get_user(id):
 def update_user(id):
     user_to_uptdate = User.get_by_id(id)
     data = user_put_args.parse_args()
+    if data['password']:
+        user_to_uptdate.password = data['password']
     if data['given_name']:
         user_to_uptdate.given_name = data['given_name']
     if data['family_name']:
@@ -286,12 +340,12 @@ def follow_user(id):
 @app.route('/api/followers/<int:id>', methods=['GET'])
 def follow_table(id):
     users = User.get_all()
-    theUser = User.get_by_id(id)
+    user_with_followers = User.get_by_id(id)
     data = []
     for user in users:
-        if user.is_following(theUser):
+        if user.is_following(user_with_followers):
             data.append(user)
-    serializer = UserSchema(many=True)
+    serializer = FollowerSchema(many=True)
     result = serializer.dump(data)
     return jsonify(result), 200
 
@@ -513,6 +567,32 @@ def not_found(error):
 @app.errorhandler(500)
 def internal_server(error):
     return jsonify({'message' : 'There is a problem'}), 500
+
+@app.cli.command("bootstrap")
+def bootstrap_data():
+    db.drop_all()
+    db.create_all()
+
+    team1 = Team('Aalesund Fotballklubb', 'AaFK', 'Norway','https://divisjonsforeningen.no/wp-content/uploads/2015/03/aalesund_logo_512.png')
+    team2 = Team('Fotballklubben Bodø/Glimt', 'B/Ø', 'Norway', 'https://divisjonsforeningen.no/wp-content/uploads/2019/01/glimt.png')
+    team3 = Team('Hamarkameratene', 'HamKam', 'Norway', 'https://www.fotballnerd.no/wp-content/uploads/2018/08/73F74FB6-E83E-4773-BE46-0A4D12A2F5CC.png')
+    team4 = Team('Fotballklubben Haugesund', 'FKH', 'Norway', 'https://seeklogo.com/images/F/fk-haugesund-logo-A0F2A7E062-seeklogo.com.png')
+    team5 = Team('Fotballklubben Jerv', 'FKJ', 'Norway', 'https://www.fkjerv.no/ffo/_/image/af79704d-b782-4c83-95ae-2fb5d4b1ad72:9a68176dc8e024252ddb98acc9b51342e2c79b4e/wide-72-72/jer-logo-fra-ai.svg')
+    team6 = Team('Kristiansund Ballklubb', 'KBK', 'Norway', 'https://www.kristiansundbk.no/_/asset/no.seeds.app.football:1631535449/img/logo/kri/logo.png')
+    team7 = Team('Lillestrøm Sportsklubb', 'LSK', 'Norway', 'https://www.h-a.no/wp-content/uploads/2020/06/lsklogo.png')
+
+    db.session.add(team1)
+    db.session.add(team2)
+    db.session.add(team3)
+    db.session.add(team4)
+    db.session.add(team5)
+    db.session.add(team6)
+    db.session.add(team7)
+
+    db.session.commit()
+
+
+    print('Added data to database')
 
 if __name__ == '__main__':
     app.run(debug=True)
