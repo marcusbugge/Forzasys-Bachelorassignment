091a8176d6c1a69baf96dab2207a05437d8a8421
Henrik Madsen <s341841@oslomet.no>
2022-03-07 17:47:39 +0100

    Styled the hover effects on badges and improved functionality. Started on functionality for editing profile information.

--- frontend/clientapp/src/components/pages/profile/Profilepage.js
+++ frontend/clientapp/src/components/pages/profile/Profilepage.js
@@ -4,22 +4,25 @@ import usericon from "../../../assets/icons/usericon.png";
 import FeedPosts from "../feed/FeedPosts";
 import axios from "axios";
 import HoverImage from "react-hover-image";
-  
+import { BiCog } from "react-icons/bi";
+import { IconContext } from "react-icons";
+
 export default function Profilepage() {
   const [user, setUser] = useState();
   const [badges, setBadges] = useState([]);
   const [loading, setLoading] = useState();
   const [display, setDisplay] = useState("badge-info-cnt-notdisplayed");
+  const [hoveredBadge, setHoveredBadge] = useState(-1);
 
-  const showBadge = e => {
-    e.preventDefault();
+  const showBadge = (index) => {
+    setHoveredBadge(index);
     setDisplay("badge-info-cnt-displayed");
-  }
+  };
 
-  const hideBadge = e => {
-    e.preventDefault();
+  const hideBadge = () => {
+    setHoveredBadge(-1);
     setDisplay("badge-info-cnt-notdisplayed");
-  }
+  };
 
   async function getUsers() {
     const test = await axios.get("http://localhost:5000/api/user");
@@ -31,7 +34,7 @@ export default function Profilepage() {
   useEffect(() => {
     getBadges();
   }, []);
-  async function getBadges(){
+  async function getBadges() {
     const henticon = await axios.get("http://localhost:5000/api/badges/user/1");
     console.log("Req: ", henticon);
     const data = henticon.data;
@@ -47,40 +50,56 @@ export default function Profilepage() {
         <div className="picture-icon-cnt">
           <img src={usericon} alt="profilepicture" />
         </div>
+        <IconContext.Provider value={{ size: "30px" }}>
+          <div className="profile-edit-cnt">
+            <BiCog />
+          </div>
+        </IconContext.Provider>
+
         <h1>Navn Navnesen</h1>
       </div>
-      
+
       <div className="badges-cnt">
-      {badges.map((icon) => (
-          <div className="badge">
-         
-            <div className="badge-img-cnt"
-                     onMouseEnter={e => {
-                      showBadge(e)
-                    }}
-                    onMouseLeave={e => {
-                      hideBadge(e)
-                    }}
+        {badges.map((icon, index) => (
+          <div key={index} className="badge">
+            <div
+              className="badge-img-cnt"
+              onMouseEnter={() => {
+                showBadge(index);
+              }}
+              onMouseLeave={(e) => {
+                hideBadge(e);
+              }}
             >
-            
               <img src={icon.picture} alt="" />
-              <img src={require("../../../assets/badgeIcons/" +icon.picture)} alt="badgeicon"/>
-          
+              <img
+                src={require("../../../assets/badgeIcons/" + icon.picture)}
+                alt="badgeicon"
+              />
             </div>
-            {display && <div className={display}>{icon.name}
-              <p>Description: {icon.description}</p>
-              <p>Points needed: {icon.points_needed}</p>
-              </div>} 
+            {hoveredBadge === index ? (
+              <div key={index} className={display}>
+                <div className="badge-title">
+                  <h3>{icon.name}</h3>
+                </div>
+                <div className="badge-desc-cnt">
+                  <div className="badge-description">
+                    <p>Description: {icon.description}</p>
+                  </div>
+                  <div className="badge-points">
+                    <p>Points needed: {icon.points_needed}</p>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
-            ))}
-
-          
+        ))}
       </div>
-
       <div className="most-popularclips-cnt">
         <div className="stroke-blue"></div>
-        <div className="posts">
-        </div>
+        <div className="posts"></div>
       </div>
     </div>
   );
