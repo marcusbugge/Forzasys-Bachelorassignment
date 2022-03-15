5194ebc318a313cf8c7f87e8f1155e56cd72a99d
marcusbugge <marcus_bugge@hotmail.com>
2022-03-15 16:01:19 +0100

    styling login screen

--- frontend/clientapp/src/components/pages/login/Alert.js
+++ frontend/clientapp/src/components/pages/login/Alert.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Loginconfirmation() {
+export default function Alert() {
   return <div className="loginconfirm-cnt">Login successful!</div>;
 }
