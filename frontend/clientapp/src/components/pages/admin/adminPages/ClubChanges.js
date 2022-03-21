74912a6c61280174383c1b483d43c1a27a96453e
s345871 <s345871@oslomet.no>
2022-03-21 18:07:15 +0100

    started with admin page

--- frontend/clientapp/src/components/pages/admin/adminPages/ClubChanges.js
+++ frontend/clientapp/src/components/pages/admin/adminPages/ClubChanges.js
@@ -7,6 +7,10 @@ export default function QuizChanges() {
   const [clubs, setClubs] = useState([]);
   const [loading, setLoading] = useState(true);
   const [disabled, setDisabled] = useState(false);
+  const [visable, setVisable] = useState({
+    element: null,
+    bool: false,
+  });
 
   const url = "http://localhost:5000/api/clubs";
   useEffect(() => {
@@ -23,15 +27,97 @@ export default function QuizChanges() {
       .then((response) => {
         setClubs(response.data);
         setLoading(false);
+        console.log(response.data);
       })
       .catch(() => {
         setDisabled(false);
       });
   }
 
+  function handleClick(index) {
+    if (visable.bool && visable.element !== index) {
+      setVisable({
+        element: index,
+        bool: true,
+      });
+    } else if(visable.bool && visable.element === index){
+      setVisable({
+        element: null,
+        bool: false,
+      });
+    } else {
+      setVisable({
+        element: index,
+        bool: true,
+      });
+    }
+  }
+
+  const Edit = () => {
+    return (
+      <div className="change-area">
+        <form className="form-clubs">
+          <input type="text" name="name" placeholder="Enter a name" />
+          <input
+            type="text"
+            name="nationality"
+            placeholder="Enter nationality"
+          />
+          <button type="submit">SAVE</button>
+        </form>
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <h2>KYS Lagene</h2>
+    <div className="admin-club-page">
+      <div className="table-clubs">
+        <div className="clubs-head">
+          <div className="club-logo">Logo</div>
+          <div className="club-name">Name</div>
+          <div className="club-nat">Nationality</div>
+          <div className="club-edit">Edit</div>
+        </div>
+
+        {clubs.map((item, index) => (
+          <div className="clubs-body" key={index}>
+            <div className="body-row">
+              <div className="club-logo-content">
+                <img
+                  className="club-img"
+                  src={require("../../../../assets/teamLogos/" + item.logo)}
+                />
+              </div>
+              <div className="club-name-content">{item.name}</div>
+              <div className="club-nat-content">{item.nationality}</div>
+              <div className="club-edit-content">
+                <button onClick={() => handleClick(index)}>EDIT</button>
+              </div>
+            </div>
+            <div className="change-box">
+              {visable.element === index && visable.bool ? <Edit /> : ""}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="addClub-form">
+        <h3>Add a club</h3>
+        <form>
+          <input
+            type="text"
+            name="name"
+            required="required"
+            placeholder="Enter a name"
+          />
+          <input
+            type="text"
+            name="nationality"
+            required="required"
+            placeholder="Enter nationality"
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
     </div>
   );
 }
--- frontend/clientapp/src/components/pages/admin/adminPages/QuizChanges.js
+++ frontend/clientapp/src/components/pages/admin/adminPages/QuizChanges.js
@@ -7,6 +7,16 @@ export default function BadgeChanges() {
   const [quiz, setQuiz] = useState([]);
   const [loading, setLoading] = useState(true);
   const [disabled, setDisabled] = useState(false);
+  const [visable, setVisable] = useState({
+    element: null,
+    bool: false,
+  });
+
+  const [answers, setAnswers] = useState({
+    bool: false,
+    list: [],
+    element: null,
+  });
 
   const url = "http://localhost:5000/api/quizes";
   useEffect(() => {
@@ -16,6 +26,10 @@ export default function BadgeChanges() {
       console.log("quiz ", quiz);
     }
   }, [quiz]);
+
+  useEffect(() => {
+    console.log(answers);
+  }, [answers]);
 
   function requestAPI() {
     axios
@@ -29,9 +43,121 @@ export default function BadgeChanges() {
       });
   }
 
+  function handleEditClick(index) {
+    if (visable.bool && visable.element === index) {
+      setVisable({
+        element: null,
+        bool: false,
+      });
+    } else if(visable.bool && visable.element !== index){
+      setVisable({
+        element: index,
+        bool: true,
+      });
+    }else {
+      setVisable({
+        element: index,
+        bool: true,
+      });
+    }
+    setAnswers({
+      bool: false,
+      list: [],
+      element: null,
+    });
+  }
+
+  function handleAnswersClick(index) {
+    if(answers.bool && answers.element !== index){
+      setAnswers({
+        bool: true,
+        list: quiz[index].answers,
+        element: index,
+      });
+    }
+    else if (answers.bool && answers.element === index) {
+      setAnswers({
+        bool: false,
+        list: [],
+        element: null,
+      });
+    } else {
+      setAnswers({
+        bool: true,
+        list: quiz[index].answers,
+        element: index,
+      });
+    }
+    setVisable({
+      element: null,
+      bool: false,
+    });
+  }
+
+  const Edit = () => {
+    return (
+      <div className="change-area">
+        <form className="form-quizs">
+          <input type="text" name="name" placeholder="Enter a name" />
+          <input
+            type="text"
+            name="description"
+            placeholder="Enter a description"
+          />
+          <input type="text" name="category" placeholder="Enter a category" />
+          <input type="text" name="points" placeholder="Enter points" />
+          <button type="delete">Delete</button>
+          <button type="submit">SAVE</button>
+        </form>
+      </div>
+    );
+  };
+
+  const Answerkys = () => {
+    return (
+      <div className="answer-box">
+        {answers.list.map((item, index) => (
+          <div key={index}>
+            Answer {index + 1} : {item}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <h2>KYS Quizene</h2>
+    <div className="admin-quiz-page">
+      <div className="table-quiz">
+        <div className="quiz-head">
+          <div className="quiz-question">Question</div>
+          <div className="quiz-points">Points</div>
+          <div className="quiz-edit">Edit</div>
+          <div className="quiz-answers">Answers</div>
+        </div>
+
+        {quiz.map((item, index) => (
+          <div className="quiz-body" key={index}>
+            <div className="body-row">
+              <div className="quiz-question-content">{item.question}</div>
+              <div className="quiz-points-content">{item.points}</div>
+              <div className="quiz-edit-content">
+                <button onClick={() => handleEditClick(index)}>EDIT</button>
+              </div>
+              <div className="quiz-answers-content">
+                <button onClick={() => handleAnswersClick(index)}>
+                  Answers
+                </button>
+              </div>
+            </div>
+            <div className="answers-view">
+              {answers.bool && answers.element === index ? <Answerkys /> : ""}
+            </div>
+            <div className="change-box">
+              {visable.element === index && visable.bool ? <Edit /> : ""}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
